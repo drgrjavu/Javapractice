6dc1377bb1f436493d3194d09ed2f3b70650b1da
Jongchan Choi <drgrjongchan@gmail.com>
2022-06-01 20:43:23 +0900

    첫번째 자바연습

---
+++ first/src/hello/HelloJava.java
@@ -0,0 +1,15 @@
+/*
+ * Date : 2022/05/21
+ * Author : 최종찬
+ * Description : 첫번째 자바 프로그램입니다.
+ * Version : 1.7
+ */
+package hello;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		System.out.println("Hello,Java");
+		//System.out.println("Hello,Java") 한 줄 주석 연습 단어출력연습
+	}
+}
