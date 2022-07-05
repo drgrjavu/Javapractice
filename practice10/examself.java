54df831741be7329d5eff12ed5317944ee25367d
Jongchan Choi <drgrjongchan@gmail.com>
2022-07-05 20:01:50 +0900

    자바연습

---
+++ practice10/examself.java
@@ -0,0 +1,25 @@
+public class examself {
+    public static void main(String[] args) {
+        String floor = "2";
+        
+        switch(floor) {
+            case "1":
+            System.out.println(floor + "층 약국입니다");
+            break;
+            case "2":
+            System.out.println(floor + "층 정형외과입니다");
+            break;
+            case "3":
+            System.out.println(floor + "층 피부과입니다");
+            break;
+            case "4":
+            System.out.println(floor + "층 치과입니다");
+            break;
+            case "5":
+            System.out.println(floor + "층 헬스 클럽입니다");
+            break;
+
+        }
+        
+    }
+}
