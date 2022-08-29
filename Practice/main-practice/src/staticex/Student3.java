88a24fe271630d99053ab247a3a107f9572c07dd
Jongchan Choi <drgrjongchan@gmail.com>
2022-08-29 18:52:06 +0900

    자바실습

--- Practice/main-practice/src/staticex/Student3.java
+++ Practice/main-practice/src/staticex/Student3.java
@@ -1,15 +1,20 @@
 package staticex;
 
-public class Student2 {
+public class Student3 {
   private static int serialNum = 1000;
+  private static int cardNum = 100;
   public int studentID;
+  public int studentCard;
   public String studentName;
   public int grade;
   public String address;
 
-  public Student2() {
+  public Student3() {
     serialNum++;
     studentID = serialNum;
+    studentCard = studentID + cardNum;
+    cardNum += 100;
+
   }
   public String getStudentName() {
     return studentName;
@@ -23,7 +28,15 @@ public class Student2 {
     return serialNum;
   }
   public static void setSerialNum(int serialNum) {
-    Student2.serialNum = serialNum;
+    Student3.serialNum = serialNum;
   }
+  public static int getCardNum() {
+    return cardNum;
+  }
+  public static void setCardNum(int cardNum) {
+    Student3.cardNum = cardNum;
+  }
+
+
 
 }
